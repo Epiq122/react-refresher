fb018167a3f5ebeaba4a4cfe90f848bd2928c582
Robert Gleason <l2ob122@gmail.com>
2023-04-04 21:41:02 -0700

    added state from app to counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 import Counter from './Counter';
 
 function App() {
+  const [check, setCheck] = useState('You need to click more!');
   return (
     <div className='App'>
-      <Counter />
+      <Counter check={check} setCheck={setCheck} />
     </div>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 
-export default function Counter() {
+export default function Counter({ check, setCheck }) {
   // Js Logic, creating functions,adding state etc
   const [counter, setCounter] = useState(0);
+
   const counterHandler = () => {
     setCounter(counter + 1);
+    if (counter >= 5) setCheck('Stop, you clicked enough');
   };
   return (
     <div>
+      {counter < 5 ? <h1>Counter is to low</h1> : <h1>Counter is higgggh</h1>}
+      <h1>{check}</h1>
+      {counter > 5 && <h1>WOOOO</h1>}
       <h1>{counter}</h1>
       <button onClick={counterHandler}>Increment</button>
     </div>
