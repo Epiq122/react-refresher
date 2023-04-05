2c51b132e6bec72f944f686826b85989c6b35c7f
Robert Gleason <l2ob122@gmail.com>
2023-04-04 22:03:10 -0700

    useEffect displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
-import Counter from './Counter';
 
 function App() {
-  const [check, setCheck] = useState('You need to click more!');
+  const [todos, setTodos] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos/',
+      );
+      const data = await response.json();
+      setTodos(data);
+      console.log('USE EFFECT RAN');
+    };
+    fetchTodos();
+  }, [count]);
+
   return (
     <div className='App'>
-      <Counter check={check} setCheck={setCheck} />
+      <h1 onClick={() => setCount(count + 1)}>{count}</h1>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h3>{todo.title}</h3>
+        </div>
+      ))}
     </div>
   );
 }
